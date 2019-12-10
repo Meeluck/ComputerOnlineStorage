1ee94d35043d98f0bae9cc3318d01306363a65b9
Meeluck <50030554+Meeluck@users.noreply.github.com>
2019-12-10 22:00:59 +0300

    Добавление категорий
    Проблемы:
    1)Не получилось отобразить родительскую категорию ее названием(вывожу только число)
    2)При удалении родительской (по отношению к другим) категории у предков значение не меняется на null

---
+++ ComputerOnlineStorage/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComputerOnlineStorage.Models
+{
+	public class Category
+	{
+		[Required, Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int CategoryId { get; set; }
+
+		public int? ParentCategory { get; set; }
+		[Required, MaxLength(75)]
+		public string CategoryName { get; set; }
+
+	}
+}
