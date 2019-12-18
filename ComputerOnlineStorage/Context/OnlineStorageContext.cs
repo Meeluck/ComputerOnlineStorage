c1195cae35d0a60d37569a0190a38061a1394ba7
Meeluck <50030554+Meeluck@users.noreply.github.com>
2019-12-18 19:16:22 +0300

    Исправлено отображение категорий

--- ComputerOnlineStorage/Context/OnlineStorageContext.cs
+++ ComputerOnlineStorage/Context/OnlineStorageContext.cs
@@ -42,6 +42,8 @@ namespace ComputerOnlineStorage.Context
 			db.Manufactures.Add(new Manufacture { ManufactureId = 7, ManufactureName = "Western Digital" });
 			db.Manufactures.Add(new Manufacture { ManufactureId = 8, ManufactureName = "Dell" });
 			db.Manufactures.Add(new Manufacture { ManufactureId = 9, ManufactureName = "HP" });
+			db.Manufactures.Add(new Manufacture { ManufactureId = 10, ManufactureName = "AMD" });
+			db.Manufactures.Add(new Manufacture { ManufactureId = 11, ManufactureName = "Samsung" });
 
 			//добавление ин-ии о катагориях
 			db.Categories.Add(new Category { CategoryId = 1, ParentCategory = null, CategoryName = "Компьютер" });
@@ -49,6 +51,12 @@ namespace ComputerOnlineStorage.Context
 			db.Categories.Add(new Category { CategoryId = 3, ParentCategory = 1, CategoryName = "Ноутбук" });
 			db.Categories.Add(new Category { CategoryId = 4, ParentCategory = 3, CategoryName = "Игровой ноутбук" });
 			db.Categories.Add(new Category { CategoryId = 5, ParentCategory = 3, CategoryName = "Ноутбук для учебы" });
+			db.Categories.Add(new Category { CategoryId = 6, ParentCategory = null, CategoryName = "Комплектуюище компьютера" });
+			db.Categories.Add(new Category { CategoryId = 7, ParentCategory = 6, CategoryName = "Процессоры" });
+			db.Categories.Add(new Category { CategoryId = 8, ParentCategory = 6, CategoryName = "Оперативная память" });
+			db.Categories.Add(new Category { CategoryId = 9, ParentCategory = 6, CategoryName = "SSD" });
+			db.Categories.Add(new Category { CategoryId = 10, ParentCategory = 6, CategoryName = "HDD" });
+			db.Categories.Add(new Category { CategoryId = 11, ParentCategory = 6, CategoryName = "Видеокарты" });
 
 
 			//добавление ин-ии о странах 
@@ -59,7 +67,7 @@ namespace ComputerOnlineStorage.Context
 			db.CountryProduces.Add(new CountryProduce { CountryId = 5, CountryName = "Индия" });
 
 			//ДОБАВЛЕНИЕ ХАРАКТЕРИСТИК
-				//основные характеристики процесссоров
+			//основные характеристики процесссоров
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 1,
@@ -77,7 +85,7 @@ namespace ComputerOnlineStorage.Context
 				CharacteristicName = "Кэш процессора",
 				Measure = "МБ"
 			});
-				//основыне характеристики дисплев
+			//основыне характеристики дисплев
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 4,
@@ -85,7 +93,7 @@ namespace ComputerOnlineStorage.Context
 			});
 			db.Characteristics.Add(new Characteristics
 			{
-				CharacteristicId = 5, 
+				CharacteristicId = 5,
 				CharacteristicName = "Диагональ экрана"
 			});
 			db.Characteristics.Add(new Characteristics
@@ -99,7 +107,7 @@ namespace ComputerOnlineStorage.Context
 				CharacteristicName = "Частота обновления экрана",
 				Measure = "Гц"
 			});
-				//основные характеристики оперативной памяти
+			//основные характеристики оперативной памяти
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 8,
@@ -117,7 +125,7 @@ namespace ComputerOnlineStorage.Context
 				CharacteristicId = 10,
 				CharacteristicName = "Тип оперативной памяти" //ddr4 or ddr3
 			});
-				//устройства хранения данных
+			//устройства хранения данных
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 11,
@@ -135,7 +143,7 @@ namespace ComputerOnlineStorage.Context
 				CharacteristicName = "Скорость работы HDD",
 				Measure = "об/мин"
 			});
-				//Видеокарта
+			//Видеокарта
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 14,
@@ -147,7 +155,7 @@ namespace ComputerOnlineStorage.Context
 				CharacteristicId = 15,
 				CharacteristicName = "Тип видеокарты" //встроенная или дискретная
 			});
-				//Опеационная система
+			//Опеационная система
 			db.Characteristics.Add(new Characteristics
 			{
 				CharacteristicId = 16,
@@ -162,7 +170,7 @@ namespace ComputerOnlineStorage.Context
 			DeviceCharacteristic dc1 = new DeviceCharacteristic
 			{
 				DeviceCharacteristicId = 1,
-				CharacteristicId = 1, 
+				CharacteristicId = 1,
 				Value = "6"
 			};
 			DeviceCharacteristic dc2 = new DeviceCharacteristic
@@ -278,9 +286,9 @@ namespace ComputerOnlineStorage.Context
 			DeviceCharacteristic dc23 = new DeviceCharacteristic
 			{
 				CharacteristicId = 14,
-				Value ="6"
+				Value = "6"
 			};
-			
+
 			List<DeviceCharacteristic> deviceCharacteristicsList1 = new List<DeviceCharacteristic>
 			{
 				dc17, dc1, dc2, dc3, dc4, dc5, dc6, dc7, dc8, dc9,
@@ -288,7 +296,7 @@ namespace ComputerOnlineStorage.Context
 				dc18
 			};
 
-			List<DeviceCharacteristic> deviceCharacteristicsList2= new List<DeviceCharacteristic>
+			List<DeviceCharacteristic> deviceCharacteristicsList2 = new List<DeviceCharacteristic>
 			{
 				dc17, dc1, dc2, dc3, dc4, dc5, dc6, dc7, dc20,dc21,dc10,dc11,
 				dc13,dc22,dc12,dc14,dc17,dc23,dc18
@@ -315,8 +323,199 @@ namespace ComputerOnlineStorage.Context
 				CountryId = 2,
 				DeviceCharacteristics = deviceCharacteristicsList2
 			};
-
 			db.Devices.AddRange(new List<Device> { dv1, dv2 });
+
+			// INTEL Core I9-9900
+			DeviceCharacteristic dcForProc1 = new DeviceCharacteristic
+			{
+				CharacteristicId = 1,
+				Value = "8"
+			};
+			DeviceCharacteristic dcForProc2 = new DeviceCharacteristic
+			{
+				CharacteristicId = 2,
+				Value = "3.1-5.0"
+			};
+			DeviceCharacteristic dcForProc3 = new DeviceCharacteristic
+			{
+				CharacteristicId = 3,
+				Value = "16"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForProc1 = new List<DeviceCharacteristic>
+			{
+				dcForProc1, dcForProc2,dcForProc3
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForProc1);
+			Device intelProc1 = new Device
+			{
+				ManufactureId = 3,
+				CategoryId = 7,
+				DeviceCharacteristics = listDeviceCharacteristicsForProc1,
+				DeviceSeries = "Core",
+				DeviceModel = "I9-9900",
+				CountryId = 2
+			};
+			db.Devices.Add(intelProc1);
+
+			// INTEL Core i7-8700
+			DeviceCharacteristic dcForProc4 = new DeviceCharacteristic
+			{
+				CharacteristicId = 1,
+				Value = "16"
+			};
+			DeviceCharacteristic dcForProc5 = new DeviceCharacteristic
+			{
+				CharacteristicId = 2,
+				Value = "3.2-4.6"
+			};
+			DeviceCharacteristic dcForProc6 = new DeviceCharacteristic
+			{
+				CharacteristicId = 3,
+				Value = "12"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForProc2 = new List<DeviceCharacteristic>
+			{
+				dcForProc4, dcForProc5,dcForProc6
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForProc2);
+			Device intelProc2 = new Device
+			{
+				ManufactureId = 3,
+				CategoryId = 7,
+				DeviceCharacteristics = listDeviceCharacteristicsForProc2,
+				DeviceSeries = "Core",
+				DeviceModel = "I7-8700",
+				CountryId = 2
+			};
+			db.Devices.Add(intelProc2);
+
+			//AMD Ryzen 7 3700X
+			DeviceCharacteristic dcForProc7 = new DeviceCharacteristic
+			{
+				CharacteristicId = 1,
+				Value = "8"
+			};
+			DeviceCharacteristic dcForProc8 = new DeviceCharacteristic
+			{
+				CharacteristicId = 2,
+				Value = "3.6-4.4"
+			};
+			DeviceCharacteristic dcForProc9 = new DeviceCharacteristic
+			{
+				CharacteristicId = 3,
+				Value = "32"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForProc3 = new List<DeviceCharacteristic>
+			{
+				dcForProc7, dcForProc8,dcForProc9
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForProc3);
+			Device amdProc1 = new Device
+			{
+				ManufactureId = 10,
+				CategoryId = 7,
+				DeviceCharacteristics = listDeviceCharacteristicsForProc3,
+				DeviceSeries = "Ryzen 7",
+				DeviceModel = "3700X",
+				CountryId = 2
+			};
+			db.Devices.Add(amdProc1);
+
+			//AMD Ryzen 5 2600X
+			DeviceCharacteristic dcForProc10 = new DeviceCharacteristic
+			{
+				CharacteristicId = 1,
+				Value = "6"
+			};
+			DeviceCharacteristic dcForProc11 = new DeviceCharacteristic
+			{
+				CharacteristicId = 2,
+				Value = "3.6-4.2"
+			};
+			DeviceCharacteristic dcForProc12 = new DeviceCharacteristic
+			{
+				CharacteristicId = 3,
+				Value = "16"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForProc4 = new List<DeviceCharacteristic>
+			{
+				dcForProc10, dcForProc11,dcForProc12
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForProc4);
+			Device amdProc2 = new Device
+			{
+				ManufactureId = 10,
+				CategoryId = 7,
+				DeviceCharacteristics = listDeviceCharacteristicsForProc3,
+				DeviceSeries = "Ryzen 5",
+				DeviceModel = "2600X",
+				CountryId = 2
+			};
+			db.Devices.Add(amdProc2);
+
+			//Kingston HyperX Predator DDR4 DIMM 8 Гб PC4-26600 1 шт.
+			DeviceCharacteristic dcForRam1 = new DeviceCharacteristic
+			{
+				CharacteristicId = 8,
+				Value = "8"
+			};
+			DeviceCharacteristic dcForRam2 = new DeviceCharacteristic
+			{
+				CharacteristicId = 9,
+				Value = "3333"
+			};
+			DeviceCharacteristic dcForRam3 = new DeviceCharacteristic
+			{
+				CharacteristicId = 10,
+				Value = "DDR4"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForRam1 = new List<DeviceCharacteristic>
+			{
+				dcForRam1,dcForRam2,dcForRam3
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForRam1);
+			Device kingstonRam1 = new Device
+			{
+				ManufactureId = 4,
+				CategoryId = 8,
+				DeviceCharacteristics = listDeviceCharacteristicsForRam1,
+				DeviceSeries = "HyperX Predator",
+				DeviceModel = "HX433C16PB3"
+			};
+			db.Devices.Add(kingstonRam1);
+
+			//Kingston HyperX Fury DDR4 DIMM 16 Гб PC4-25600 1 шт. (HX432C16FB3 / 16)
+			DeviceCharacteristic dcForRam4 = new DeviceCharacteristic
+			{
+				CharacteristicId = 8,
+				Value = "16"
+			};
+			DeviceCharacteristic dcForRam5 = new DeviceCharacteristic
+			{
+				CharacteristicId = 9,
+				Value = "3200"
+			};
+			DeviceCharacteristic dcForRam6 = new DeviceCharacteristic
+			{
+				CharacteristicId = 10,
+				Value = "DDR4"
+			};
+			List<DeviceCharacteristic> listDeviceCharacteristicsForRam2 = new List<DeviceCharacteristic>
+			{
+				dcForRam4,dcForRam5,dcForRam6
+			};
+			db.DeviceCharacteristics.AddRange(listDeviceCharacteristicsForRam2);
+			Device kingstonRam2 = new Device
+			{
+				ManufactureId = 4,
+				CategoryId = 8,
+				DeviceCharacteristics = listDeviceCharacteristicsForRam2,
+				DeviceSeries = " HyperX Fury",
+				DeviceModel = "HX432C16FB"
+			};
+			db.Devices.Add(kingstonRam2);
+
+
 			base.Seed(db);
 		}
 	}
