8330ca6fa6d1baa526304e84d1673a4e7305a899
Meeluck <50030554+Meeluck@users.noreply.github.com>
2019-12-24 08:38:42 +0300

    Подправлена верстка

--- ComputerOnlineStorage/App_Start/RouteConfig.cs
+++ ComputerOnlineStorage/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComputerOnlineStorage
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Device", action = "ShowDevices", id = UrlParameter.Optional }
             );
         }
     }
